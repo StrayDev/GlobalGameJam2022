f8018e4e8dd3db5c487d2fcf8ca210db5d145bd1
kieranbow <Kieran2.Bowsher@live.uwe.ac.uk>
2022-01-29 13:05:17 +0000

    Added PostProcessing.cs and SinCity_IES shader

--- Assets/PlayerTrain.cs
+++ Assets/PlayerTrain.cs
@@ -45,7 +45,7 @@ public class PlayerTrain : MonoBehaviour
         if (ghostParent && ghostsAttached.Contains(ghostParent) == false)
         {
             ghostParent.transform.SetParent(transform);
-            ghostParent.transform.position = ghostAttachPoint.position - (transform.forward * ghostSpacing * Mathf.Ceil(numGhosts/2) + (transform.right * (numGhosts % 2)));
+            ghostParent.transform.position = ghostAttachPoint.position - (transform.forward * ghostSpacing * Mathf.Ceil(numGhosts / 2.0f) + (transform.right * (numGhosts % 2.0f)));
             ghostParent.GetComponentInChildren<Collider>().enabled = false;
             numGhosts++;
             ghostsAttached.Add(ghostParent);
